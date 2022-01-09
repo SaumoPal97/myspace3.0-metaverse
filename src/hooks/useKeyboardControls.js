b7aa944376fe4107129ba26dee593ad0885a316c
SaumoPal97 <saumopal1997@gmail.com>
2022-01-10 00:16:07 +0530

    init

---
+++ src/hooks/useKeyboardControls.js
@@ -0,0 +1,91 @@
+import { useState, useEffect } from "react";
+import { useStore } from "./useStore";
+
+function actionByKey(key) {
+  const keys = {
+    KeyW: "moveForward",
+    KeyS: "moveBackward",
+    KeyA: "moveLeft",
+    KeyD: "moveRight",
+    Space: "jump",
+  };
+  return keys[key];
+}
+
+function textureByKey(key, texture) {
+  const textures = [
+    "dirt",
+    "grass",
+    "glass",
+    "wood",
+    "log",
+    "block",
+    "nft1",
+    "nft2",
+  ];
+  let newTextureIndex;
+  console.log("prev ", texture);
+  console.log("prev index ", textures.indexOf(texture));
+  if (key === "ArrowUp") {
+    newTextureIndex =
+      textures.indexOf(texture) != -1
+        ? (textures.indexOf(texture) + 1) % textures.length
+        : 0;
+  }
+  if (key === "ArrowDown") {
+    newTextureIndex =
+      textures.indexOf(texture) != -1
+        ? (textures.indexOf(texture) - 1 + textures.length) % textures.length
+        : 0;
+  }
+  console.log(textures[newTextureIndex]);
+  return textures[newTextureIndex];
+}
+export const useKeyboardControls = () => {
+  const [movement, setMovement] = useState({
+    moveForward: false,
+    moveBackward: false,
+    moveLeft: false,
+    moveRight: false,
+    jump: false,
+  });
+  const [setTexture] = useStore((state) => [state.setTexture]);
+  let [texture] = useStore((state) => [state.texture]);
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      console.log(e.code);
+      // Movement key
+      if (actionByKey(e.code)) {
+        setMovement((state) => ({
+          ...state,
+          [actionByKey(e.code)]: true,
+        }));
+      }
+      // Change texture key
+      if (e.code === "ArrowUp" || e.code === "ArrowDown") {
+        const newTexture = textureByKey(e.code, texture);
+        setTexture(newTexture);
+        texture = newTexture;
+      }
+    };
+    const handleKeyUp = (e) => {
+      if (actionByKey(e.code)) {
+        setMovement((state) => ({
+          ...state,
+          [actionByKey(e.code)]: false,
+        }));
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [setTexture]);
+
+  return movement;
+};
---
+++ src/textures.js
@@ -0,0 +1,37 @@
+import dirtImg from "./images/dirt.jpg";
+import grassImg from "./images/grass.jpg";
+import glassImg from "./images/glass.png";
+import logImg from "./images/log.jpg";
+import woodImg from "./images/wood.png";
+import blockImg from "./images/block.png";
+import { TextureLoader, NearestFilter, LinearMipMapLinearFilter } from "three";
+
+export const dirt = new TextureLoader().load(dirtImg);
+export const grass = new TextureLoader().load(grassImg);
+export const glass = new TextureLoader().load(glassImg);
+export const wood = new TextureLoader().load(woodImg);
+export const log = new TextureLoader().load(logImg);
+export const block = new TextureLoader().load(blockImg);
+export const nft1 = new TextureLoader().load(
+  "https://bafybeiczeprpyktbhkjpk5eptkimpcgrikunfg4qiqcivhzmqhx5seqtye.ipfs.infura-ipfs.io/"
+);
+export const nft2 = new TextureLoader().load(
+  "https://bafybeidmom45vu5vwrm2rqu6vxruy5f7wrugllhqfufhqmm6dknpwbbo3a.ipfs.infura-ipfs.io/"
+);
+
+dirt.magFilter = NearestFilter;
+dirt.minFilter = LinearMipMapLinearFilter;
+grass.magFilter = NearestFilter;
+grass.minFilter = LinearMipMapLinearFilter;
+glass.magFilter = NearestFilter;
+glass.minFilter = LinearMipMapLinearFilter;
+wood.magFilter = NearestFilter;
+wood.minFilter = LinearMipMapLinearFilter;
+log.magFilter = NearestFilter;
+log.minFilter = LinearMipMapLinearFilter;
+block.magFilter = NearestFilter;
+block.minFilter = LinearMipMapLinearFilter;
+nft1.magFilter = NearestFilter;
+nft1.minFilter = LinearMipMapLinearFilter;
+nft2.magFilter = NearestFilter;
+nft2.minFilter = LinearMipMapLinearFilter;
